61332151a42650d5bc6290f6d9bad31c798f7c4e
Edgar KAMDEM <e.kamdem@pickup-services.com>
2023-12-31 15:05:53 +0100

    setting up dev  mode

--- app.py
+++ app.py
@@ -292,5 +292,4 @@ def get_countries(country_iso2):
         Helper.logError(e, db, Log, request) 
 
 if __name__ == '__main__':
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+    app.run(debug=True, host="0.0.0.0")
